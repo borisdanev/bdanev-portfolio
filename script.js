ef6a4115a00f6dfed9a1d48e4e1e2857b93dcb7b
borisdanev <borisdanev9@gmail.com>
2023-12-12 16:53:19 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,76 @@
+import { object, string } from "yup";
+import isEmail from "validator/lib/isEmail";
+const form = document.querySelector("form");
+const inputs = form.querySelectorAll(".form-control");
+const websiteLinks = document.querySelectorAll(".website-link");
+const schema = object({
+  firstName: string()
+    .min(3, "Min lenght is 3")
+    .required("Please fill this field"),
+  email: string().test(
+    "is-valid",
+    () => "Invalid email",
+    (value) => isEmail(value)
+  ),
+  message: string().required("Please fill this field"),
+});
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const data = {
+    firstName: inputs[0].value,
+    email: inputs[1].value,
+    message: inputs[2].value,
+  };
+  try {
+    await schema.validate(data, { abortEarly: false });
+    const formData = new FormData(e.target);
+    await fetch(e.target.action, {
+      method: "POST",
+      body: formData,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    inputs.forEach((input) => {
+      const parent = input.parentElement;
+      const errorElements = parent.querySelectorAll(".error-message");
+      errorElements.forEach((el) => parent.removeChild(el));
+      input.value = "";
+    });
+    const messageEl = form.querySelector(".success-message");
+    messageEl.classList.add("active-message");
+    setTimeout(() => messageEl.classList.remove("active-message"), 1000);
+  } catch (err) {
+    inputs.forEach((input) => {
+      const parent = input.parentElement;
+      const id = input.id.replace(/[-_]+(.)/g, (_, character) =>
+        character.toUpperCase()
+      );
+      const errorElements = [...parent.querySelectorAll(".error-message")];
+      const errors = [...err.inner]
+        .map((item) => ({
+          path: item.path,
+          message: item.message,
+        }))
+        .filter((item) => item.path === id);
+      errorElements.forEach((el) => parent.removeChild(el));
+      errors.forEach((error) =>
+        parent.insertAdjacentHTML(
+          "beforeend",
+          `<p class="text-danger error-message">${error.message}</p>`
+        )
+      );
+    });
+  }
+};
+form.addEventListener("submit", handleSubmit);
+websiteLinks.forEach((item) =>
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    const url = item.href;
+    window.open(url, "_blank");
+  })
+);
+document
+  .querySelectorAll(".blurry")
+  .forEach((item) => item.classList.remove("blurry"));
